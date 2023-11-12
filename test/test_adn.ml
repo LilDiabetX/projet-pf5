f787414381efb23ce04bb65f0233c1c2e04e349a
DECKER benjamin <benjamin.decker@etu.u-paris.fr>
2023-11-12 15:15:27 +0100

    slices_between

--- test/test_adn.ml
+++ test/test_adn.ml
@@ -27,3 +27,7 @@ assert(cut_prefix [] [] = Some []);;
 assert(first_occ [1; 2] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([1; 1], [3; 4; 1; 2]));;
 assert(first_occ [1; 1] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([], [1; 2; 3; 4; 1; 2]));;
 assert(first_occ [1; 3] [1; 1; 1; 2; 3; 4; 1; 2] = None);;
+
+assert(slices_between [3; 3] [4; 4] [1; 1; 2; 3; 3; 1; 4; 1; 2] = []);;
+assert(slices_between [1; 2] [4; 1] [1; 1; 2; 3; 2; 1; 4; 1; 2] = [[3; 2 ;1]]);;
+assert(slices_between [A] [G] [A; C; T; G; G; A; C; T; A; T; G; A; G] = [[C; T]; [C; T; A; T]; []]);;
