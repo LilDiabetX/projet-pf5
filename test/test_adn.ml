0ced494456e80cf4abe0cd79c7fdfb3aaea172f1
DECKER benjamin <benjamin.decker@etu.u-paris.fr>
2023-11-14 09:15:58 +0100

    Tests

--- test/test_adn.ml
+++ test/test_adn.ml
@@ -1,50 +1,158 @@
-open Adn;;
+open Adn
+open Test_base
 
-(* Tests de la fonction explode*)
-assert(explode "" = []);;
-assert(explode "Hello!" = ['H';'e';'l';'l';'o';'!']);;
+let () = add_tests1
+  "3.0"
+  "explode"
+  explode
+  Fmt.string
+  Alcotest.(list char)
+  [ "", [];
+    "a" , ['a'];
+    "ab" , ['a'; 'b'];
+    "abc" ,['a'; 'b'; 'c'];
+  ]
 
-(* Tests de la fonction base_of_char*)
-assert(base_of_char 'A' = A);;
-assert(base_of_char 'C' = C);;
-assert(base_of_char 'T' = T);;
-assert(base_of_char 'G' = G);;
-assert(base_of_char '.' = WC);;
+(* V. le pp ne peut être que nop... sauf à écrire ici la fonction demandée *)
+let () = add_tests1
+  "3.1"
+  "base_of_char"
+  base_of_char
+  Fmt.char
+  (Alcotest.testable Fmt.nop (=))
+  [ 'A', A;
+    'T', T;
+    'C', C;
+    'G', G;
+    '.', WC;
+    '$', WC
+  ]
+       
+let () = add_tests1
+  "3.2"
+  "dna_of_string"
+  dna_of_string
+  Fmt.string
+  (Alcotest.testable Fmt.nop (=))
+  [ "", [];
+    "ACTG.", [A; C; T; G; WC];
+    "$GTCA", [WC; G; T; C; A]
+  ]
 
-(* Tests de la fonction dna_of_string*)
-assert(dna_of_string "GTAA..CT" = [G;T;A;A;WC;WC;C;T]);;
-assert(dna_of_string "" = []);;
-assert(dna_of_string "ZxY" = [WC;WC;WC]);;
+let () = add_tests1
+  "3.3"
+  "string_of_dna"
+  string_of_dna
+  Fmt.nop
+  (Alcotest.string)
+  [ [], "";
+    [A; C; T; G; WC], "ACTG.";
+    [WC; G; T; C; A], ".GTCA" 
+  ]
 
-(* Tests de la fonction string_of_dna*)
-assert(string_of_dna [G; T; A; A; WC; WC; C; T] = "GTAA..CT");;
-assert(string_of_dna [] = "");;
+let () = add_tests2
+  "3.4"
+  "cut_prefix"
+  cut_prefix
+  pp_list_char
+  pp_list_char
+  Alcotest.(option @@ list char)
+  begin
+    let abc = !!"abc" in
+    [ [], [], Some ([]);
+      [], abc, Some (abc);
+      !!"a", abc, Some (!!"bc");
+      !!"ab", abc, Some (!!"c");
+      abc, abc, Some (!!"");
+      !!"dc", abc, None;
+      !!"adc", abc, None;
+      !!"abd", abc, None;
+    ]
+  end
 
-(* Tests de la fonction cut_prefix*)
-assert(cut_prefix [1; 2; 3] [1; 2; 3; 4] = Some [4]);;
-assert(cut_prefix [1; 2; 3; 4] [1; 2; 3; 4] = Some []);;
-assert(cut_prefix [1; 2; 0] [1; 2; 3; 4] = None);;
-assert(cut_prefix [A; G; T] [A; G; T; C; A] = Some ([C; A]));;
-assert(cut_prefix [] [1; 2; 3] = Some ([1;2;3]));;
-assert(cut_prefix [1; 2; 0] [] = None);;
-assert(cut_prefix [] [] = Some []);;
+let () = add_tests2
+  "3.5"
+  "first_occ"
+  first_occ
+  pp_list_char
+  pp_list_char
+  Alcotest.(option @@ pair (list char) (list char))
+  begin
+    let abcd = !!"abcd" in
+    [ [], [], Some ([], []);
+      [], abcd, Some ([], abcd);
+      !!"a", abcd, Some ([], !!"bcd");
+      !!"ab", abcd, Some ([], !!"cd");
+      !!"aa", !!"abaaacd", Some (!!"ab", !!"acd");
+      !!"aaa", !!"abaaacd", Some (!!"ab", !!"cd");
+      !!"be", abcd, None;
+      !!"cde", abcd, None;
+    ]
+  end
 
-(* Tests de la fonction first_occ*)
-assert(first_occ [1; 2] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([1; 1], [3; 4; 1; 2]));;
-assert(first_occ [1; 1] [1; 1; 1; 2; 3; 4; 1; 2] = Some ([], [1; 2; 3; 4; 1; 2]));;
-assert(first_occ [1; 3] [1; 1; 1; 2; 3; 4; 1; 2] = None);;
+let () = add_tests3
+  "3.6"
+  "slices_between"
+  slices_between
+  pp_list_char
+  pp_list_char
+  pp_list_char
+  Alcotest.(list (list char))
+  [ !!"ab", !!"cd", !!"ddcdeeffggabhhh", [];
+    !!"ab", !!"cd", !!"ddcdeeffggcdhhh", [];
+    !!"ab", !!"cd", !!"ddabeeffggabhhh", [];
+    !!"ab", !!"cd", !!"ddabeecdffcdggabhhh", [!!"ee"];
+    !!"ab", !!"cd", !!"ddabeecdffabggcdhh", [!!"ee"; !!"gg"];
+    !!"ab", !!"cd", !!"ddabeecdffcdffabggcdhh", [!!"ee"; !!"gg"];
+    !!"ab", !!"cd", !!"ddabeabecdffcdffabggcdhh", [!!"eabe"; !!"gg"];
+    !!"aa", !!"cc", !!"ddaaaeaaecccffaaaffaaaggccchh", [!!"aeaae"; !!"affaaagg"];
+  ]
 
-(* Tests de la fonction slices_between*)
-assert(slices_between [3; 3] [4; 4] [1; 1; 2; 3; 3; 1; 4; 1; 2] = []);;
-assert(slices_between [1; 2] [4; 1] [1; 1; 2; 3; 2; 1; 4; 1; 2] = [[3; 2 ;1]]);;
-assert(slices_between [A] [G] [A; C; T; G; G; A; C; T; A; T; G; A; G] = [[C; T]; [C; T; A; T]; []]);;
+let () = add_tests1
+  "3.7"
+  "(fun s -> List.map string_of_dna (cut_genes (dna_of_string s)))"
+  (fun s -> List.map string_of_dna (cut_genes (dna_of_string s)))
+  Fmt.string
+  Alcotest.(list string)
+  [ "", [];
+    "ATGTAA", [""];
+    "AATGTAAA", [""];
+    "AATGATGTAATAAA", ["ATG"];
+    "AATGTAATAAATGATATAA", [""; "ATA"];
+    "AATGTAATAAATGATATAATAATGTG..TATAAA", [""; "ATA"; "TG..TA"];
+    "AATTAATATGATGATGATAATAATGTG..TATAAA", ["ATGATGA"; "TG..TA"]
+  ]
 
-(* Test de la fonction cut_genes*)
-let strand = dna_of_string "ATGCCTGGGCATTGAGATCATTGGCACCCTGCATAAGATGTGTGACTGTAGAGCTCTTCCTGACCATGCATAAAGAATG.CCAATGGCACAGCTGGTATC..TTTGCCATAAATGGCTCCTGGTGGAGCTGATAGTCACTTTCCATAATTAATGCATGGTGGTGGAGTTATTCTTGACTTTCCATAA";;
-let genes = List.map (string_of_dna) (cut_genes strand);;
-assert(genes = ["CCTGGGCATTGAGATCATTGGCACCCTGCA";"TGTGACTGTAGAGCTCTTCCTGACCATGCA";".CCAATGGCACAGCTGGTATC..TTTGCCA";"GCTCCTGGTGGAGCTGATAGTCACTTTCCA";"CATGGTGGTGGAGTTATTCTTGACTTTCCA"]);;
+let char_consensus_to_string = function
+  | Full c -> Printf.sprintf "Full %c" c
+  | Partial (c, n) -> Printf.sprintf "Partial (%c, %n)" c n
+  | No_consensus -> "No_consensus"
+let pp_consensus_answer ppf answer =
+  Format.pp_print_string ppf @@ char_consensus_to_string answer
 
-(* Tests de la fonction consensus*)
-assert(consensus [1; 1; 1; 1] = Full 1);;
-assert(consensus [1; 1; 1; 2] = Partial (1, 3));;
-assert(consensus [1; 1; 2; 2] = No_consensus);;
+let () = add_tests1
+  "3.8"
+  "consensus"
+  consensus
+  pp_list_char
+  (Alcotest.testable pp_consensus_answer (=))
+  [ !!"aaaa", Full 'a';
+    !!"abacca", Partial ('a', 3); 
+    !!"abccba", No_consensus;
+    [], No_consensus
+  ]
+
+let () = add_tests1
+  "3.9"
+  "consensus_sequence"
+  consensus_sequence
+  pp_list_list_char
+  Alcotest.(list (Alcotest.testable pp_consensus_answer (=)))
+  [ [], [];
+    [!!"acgt"], [Full 'a'; Full 'c'; Full 'g'; Full 't'];
+    [!!"acgt"; !!"agg."], [Full 'a'; No_consensus; Full 'g'; No_consensus];
+    [!!"aaaa"; !!"aaat"; !!"aatt"; !!"attt"],
+    [Full 'a'; Partial ('a', 3); No_consensus; Partial ('t', 3)];
+    [!!"aaagt"; !!"agatt"; !!"a.ttt"; !!"gtttt"],
+    [Partial ('a', 3); No_consensus; No_consensus; Partial ('t', 3); Full 't']
+  ]
